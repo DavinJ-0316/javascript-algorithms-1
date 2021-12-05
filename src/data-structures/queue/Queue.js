d94da7ddbf19c7056aeb379c9aa25b71abe87dcc
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2021-12-05 12:57:02 +0300

    Добавлена структура данных Очередь

---
+++ src/data-structures/queue/Queue.js
@@ -0,0 +1,30 @@
+import LinkedList from '../linked-list/LinkedList';
+
+export default class Queue {
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  isEmpty() {
+    return this.linkedList.isEmpty();
+  }
+
+  peek() {
+    if (this.linkedList.isEmpty()) return null;
+
+    return this.linkedList.head.value;
+  }
+
+  enqueue(value) {
+    this.linkedList.append(value);
+  }
+
+  dequeue() {
+    const removedHead = this.linkedList.deleteHead();
+    return removedHead ? removedHead.value : null;
+  }
+
+  toString(callback) {
+    return this.linkedList.toString(callback);
+  }
+}
