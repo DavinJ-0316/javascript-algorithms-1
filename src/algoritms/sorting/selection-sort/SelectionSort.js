e7dbe38e3eb6558a5522a2f2f3090cb5ff755dbb
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2021-12-05 17:59:11 +0300

    Добавлена сортировка выбором

---
+++ src/algoritms/sorting/selection-sort/SelectionSort.js
@@ -0,0 +1,11 @@
+function selectionSort(array) {
+  for (let partIndex = array.length - 1; partIndex > 0; partIndex++) {
+    let largestAt = 0;
+    for (let i = 0; i < partIndex; i++) {
+      if (array[i] > array[largestAt]) {
+        largestAt = i;
+      }
+    }
+    [array[largestAt], array[partIndex]] = [array[partIndex], array[largestAt]];
+  }
+}
