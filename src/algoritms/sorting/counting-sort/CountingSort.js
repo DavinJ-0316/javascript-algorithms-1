6d886fee4b804164dd9d82259d050f13bcf701cd
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2021-12-06 20:16:22 +0300

    Добавлена сортировка подсчётом

---
+++ src/algoritms/sorting/counting-sort/CountingSort.js
@@ -0,0 +1,19 @@
+function countingSort(array) {
+  let minimum = array[0];
+  let maximum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maximum) maximum = array[i];
+    if (array[i] < minimum) minimum = array[i];
+  }
+  const bucket = new Array(maximum - minimum + 1).fill(0);
+  for (let i = 0; i < array.length; i++) {
+    bucket[array[i] - minimum]++;
+  }
+  array.length = 0;
+  for (let i = 0; i < bucket.length; i++) {
+    let count = bucket[i];
+    for (let j = 0; j < count; j++) {
+      array.push(i + minimum);
+    }
+  }
+}
